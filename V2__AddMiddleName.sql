671df0a4f2043169d721c2773425838b43d8755c
Klaus Hviid <klahvi01@easv365.dk>
2025-03-09 19:42:58 +0100

    Added middlename and updated the sql

---
+++ V2__AddMiddleName.sql
@@ -0,0 +1,37 @@
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Student')
+BEGIN
+    CREATE TABLE Student (
+        Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+        FirstName NVARCHAR(50) NOT NULL,
+        LastName NVARCHAR(50) NOT NULL,
+        Email NVARCHAR(100) NOT NULL UNIQUE,
+        EnrollmentDate DATE NOT NULL,
+        MiddleName NVARCHAR(50) NULL
+    );
+END ELSE BEGIN
+    IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('Student') AND name = 'MiddleName')
+    BEGIN
+        ALTER TABLE Student ADD MiddleName NVARCHAR(50) NULL;
+    END;
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Course')
+BEGIN
+    CREATE TABLE Course (
+        Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+        Title NVARCHAR(100) NOT NULL,
+        Credits INT NOT NULL
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Enrollment')
+BEGIN
+    CREATE TABLE Enrollment (
+        Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+        StudentId UNIQUEIDENTIFIER NOT NULL,
+        CourseId UNIQUEIDENTIFIER NOT NULL,
+        Grade NVARCHAR(2) NULL,
+        FOREIGN KEY (StudentId) REFERENCES Student(Id),
+        FOREIGN KEY (CourseId) REFERENCES Course(Id)
+    );
+END;
